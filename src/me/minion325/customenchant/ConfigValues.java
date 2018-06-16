e7ebef9b48f8848bd82e4920d57f1f40f4634fdc
minion325 <saifmohammed966@gmail.com>
2018-06-16 15:56:18 -0400

    added new config values

--- src/me/minion325/customenchant/ConfigValues.java
+++ src/me/minion325/customenchant/ConfigValues.java
@@ -6,10 +6,13 @@ public class ConfigValues {
 
     private static Main plugin = Main.getPlugin(Main.class);
 
-    public static String NO_PERMISSION;
+    public static String NO_PERMISSION, NO_SUCH_ENCHANT, ENCHANT_ADDED;
 
     public static void loadConfig(){
-        NO_PERMISSION = ChatColor.translateAlternateColorCodes('&' , plugin.getConfig().getString("no-permission", "&4You do not have permission to execute that command"));
+        NO_PERMISSION = ChatColor.translateAlternateColorCodes('&' , plugin.getConfig().getString("no-permission", "&4You do not have permission to execute that command."));
+        NO_SUCH_ENCHANT = ChatColor.translateAlternateColorCodes('&' , plugin.getConfig().getString("no-such-enchant", "&4That enchantment does not exist."));
+        ENCHANT_ADDED = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("enchant-added", "&b%enchant% %level% was successfully added to the item in hand."));
+
     }
 
 }
