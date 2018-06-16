2085553fb92f332e38d88b7101b8bb48c87eac0e
minion325 <saifmohammed966@gmail.com>
2018-06-16 13:25:09 -0400

    added /enchant command

---
+++ src/me/minion325/customenchant/commands/Enchant.java
@@ -0,0 +1,14 @@
+package me.minion325.customenchant.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Enchant implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
+
+}
