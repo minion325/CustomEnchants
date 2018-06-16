993e99f824aac7f531f196ceb41a781554763ac8
minion325 <saifmohammed966@gmail.com>
2018-06-16 13:28:10 -0400

    registered commands and listeners

--- src/me/minion325/customenchant/Main.java
+++ src/me/minion325/customenchant/Main.java
@@ -1,12 +1,17 @@
 package me.minion325.customenchant;
 
+import me.minion325.customenchant.commands.Enchant;
+import me.minion325.customenchant.commands.Enchants;
 import me.minion325.customenchant.enchanting.EnchantsManager;
+import me.minion325.customenchant.listeners.PluginEnableListener;
 
 public class Main extends CustomEnchantPlugin {
 
     @Override
     public void onEnable() {
-
+        this.getCommand("enchants").setExecutor(new Enchants());
+        this.getCommand("enchant").setExecutor(new Enchant());
+        this.getServer().getPluginManager().registerEvents(new PluginEnableListener(), this);
     }
 
     @Override
