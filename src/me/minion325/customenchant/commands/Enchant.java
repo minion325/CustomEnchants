684eb441c086234142fdbc754e24d4afbee43817
minion325 <saifmohammed966@gmail.com>
2018-06-16 17:12:43 -0400

    coded /enchant command (incomplete)

--- src/me/minion325/customenchant/commands/Enchant.java
+++ src/me/minion325/customenchant/commands/Enchant.java
@@ -1,14 +1,54 @@
 package me.minion325.customenchant.commands;
 
+import me.minion325.customenchant.ConfigValues;
+import me.minion325.customenchant.enchanting.EnchantUtils;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Enchant implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        return false;
+    public boolean onCommand(CommandSender sender, Command cmd, String name, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("You must be a player to use that");
+            return true;
+        }
+        Player player = (Player) sender;
+        if (!player.hasPermission("enchantapi.enchant")) {
+            player.sendMessage(ConfigValues.NO_PERMISSION);
+            return true;
+        }
+        if (args.length == 0) {
+            //TODO SEND USAGE
+            return true;
+        }
+        if (args.length == 1) {
+            //TODO SEND USAGE
+            return true;
+        }
+        if (player.getItemInHand() == null) {
+            player.sendMessage("You must hold an item in hand to perform this command");
+            return true;
+        }
+        try {
+            me.minion325.customenchant.enchanting.Enchant enchant;
+            if ((enchant = EnchantUtils.parseEnchant(args[1].replace('_', ' '))) == null) {
+                player.sendMessage(ConfigValues.NO_SUCH_ENCHANT);
+                return true;
+            }
+            int level = Integer.parseInt(args[1]);
+            EnchantUtils.addEnchant(player.getItemInHand(), enchant, level);
+            player.sendMessage(ConfigValues.ENCHANT_ADDED);
+            return true;
+        } catch (NumberFormatException e) {
+            player.sendMessage(ChatColor.RED + args[1] + " is not a valid enchantment level");
+            return true;
+        }
+
+
     }
 
 }
