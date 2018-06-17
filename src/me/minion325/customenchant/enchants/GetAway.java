6451809236df92b5246e458a22fe2990ca092a13
minion325 <saifmohammed966@gmail.com>
2018-06-17 19:40:37 -0400

    added another enchant for demonstartion of what can be done with this api

--- src/me/minion325/customenchant/enchants/GetAway.java
+++ src/me/minion325/customenchant/enchants/GetAway.java
@@ -1,4 +1,23 @@
 package me.minion325.customenchant.enchants;
 
-public class GetAway {
+import me.minion325.customenchant.enchanting.Enchant;
+import me.minion325.customenchant.enchanting.MaterialType;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+import java.util.Random;
+
+public class GetAway extends Enchant {
+
+    public GetAway(){
+        super("Get Away", 3, MaterialType.CHESPLATE, MaterialType.LEGGINGS);
+    }
+
+    @Override
+    public void onGetHit(EntityDamageEvent event, int level) {
+        if (!(event instanceof EntityDamageByEntityEvent)) return;
+        EntityDamageByEntityEvent e = ((EntityDamageByEntityEvent) event);
+        // COMMENTED FOR TESTING PURPOSESif (!(new Random().nextInt(500) < level)) return;
+        ((EntityDamageByEntityEvent) event).getDamager().setVelocity(((EntityDamageByEntityEvent) event).getDamager().getLocation().getDirection().normalize().multiply(-3));
+    }
 }
