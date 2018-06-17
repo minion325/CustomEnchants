85c8652ad94fb068cf16f2e02cf32c1263204fc1
minion325 <saifmohammed966@gmail.com>
2018-06-16 23:26:47 -0400

    added a custom enchant for testing purposes

--- src/me/minion325/customenchant/enchants/Death.java
+++ src/me/minion325/customenchant/enchants/Death.java
@@ -2,24 +2,21 @@ package me.minion325.customenchant.enchants;
 
 import me.minion325.customenchant.enchanting.Enchant;
 import me.minion325.customenchant.enchanting.MaterialType;
-import org.bukkit.Effect;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
-public class Blinded extends Enchant {
+public class Death extends Enchant {
 
-    public Blinded() {
-        super("Blinded", 5, MaterialType.SWORD, MaterialType.AXE);
+    public Death() {
+        super("Death", 5, MaterialType.SWORD, MaterialType.AXE);
     }
 
     @Override
     public void onHit(EntityDamageByEntityEvent event, int level) {
         if (event.getEntity() instanceof LivingEntity){
-            LivingEntity entity = ((LivingEntity) event.getEntity());
-            entity.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, level * 20, 0));
-            entity.getWorld().playEffect(entity.getEyeLocation(), Effect.SMOKE, 0);
+            ((LivingEntity) event.getEntity()).setHealth(0);
+            event.getDamager().sendMessage(ChatColor.GRAY + "Your opponent is dead");
         }
     }
 }
